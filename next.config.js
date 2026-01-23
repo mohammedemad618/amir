799fbab13ffd2ea32ffd66d5fd958813f0c66dad
Mohammed <mohammed@example.com>
2026-01-23 19:02:48 +0800

    fix: database configuration for postgresql and update CSP for butterflies image

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://images.unsplash.com https://ibb.co;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https://images.unsplash.com https://ibb.co https://assets.codepen.io;",
           },
         ],
       },
